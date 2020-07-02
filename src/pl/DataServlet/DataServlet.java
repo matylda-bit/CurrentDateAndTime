e1f3e01def874e041a5738aa05e81556366419d3
matylda-bit <58633008+matylda-bit@users.noreply.github.com>
2020-07-02 13:54:02 +0100

    Add AktualnaData i Czas website files

---
+++ src/pl/DataServlet/DataServlet.java
@@ -0,0 +1,32 @@
+package pl.DataServlet;
+
+import javax.lang.model.type.TypeMirror;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+
+@WebServlet("/DataServlet")
+public class DataServlet extends HttpServlet {
+
+    @Override
+    // By ANY means that's not the way of creating views!
+    // But until we reach JSP I'll stick to it.
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        PrintWriter responseOutput = response.getWriter();
+
+
+    }
+}
